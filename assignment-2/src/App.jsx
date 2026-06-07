5fc43d299248c6bcc07c64b09ec3d695e9bb7aa1
YuJin Song <syujin2323@gmail.com>
2026-06-07 12:23:10 +0900

    chore: Vite + React + Tailwind CSS v4 프로젝트 셋업

---
+++ assignment-2/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-blue-600">
+      Tailwind 작동 확인
+    </h1>
+  )
+}
+
+export default App
